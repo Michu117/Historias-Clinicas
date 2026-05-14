919aef6b228f6843b429a763211adcb015e6df11
Aree17 <arelys.ajila@unl.edu.ec>
2026-05-13 20:07:41 -0500

    Ajustes finales Reportes
    se corrigieron las vistas para generar reportes basados en datos reales

--- HistoriasClinicas/HistoriasClinicas/urls.py
+++ HistoriasClinicas/HistoriasClinicas/urls.py
@@ -42,7 +42,7 @@ urlpatterns = [
     path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair_v1'),
     path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh_v1'),
     path('api/v1/auth/', include('Seguridad.urls')),
-    path('api/v1/historias/', include('Historias.urls')),
+    path('api/v1/Historias/', include('Historias.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
