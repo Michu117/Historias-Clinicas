17c7439bfe76da1533c28c0b39581e4070125375
Jasiel <jasiel.quezada@unl.edu.ec>
2026-06-21 18:12:47 -0500

    Problem de vivi

--- HistoriasClinicas/Historias/apps.py
+++ HistoriasClinicas/Historias/apps.py
@@ -4,5 +4,5 @@ from django.apps import AppConfig
 
 class HistoriasConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "historias"
+    name = "Historias"
     path = str(Path(__file__).resolve().parent)
