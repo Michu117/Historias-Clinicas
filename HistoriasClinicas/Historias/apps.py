acd5c89074dd6e5f886c851bfb9f663e85112c24
Jasiel <jasiel.quezada@unl.edu.ec>
2026-06-21 22:46:57 -0500

    Problem de v

--- HistoriasClinicas/Historias/apps.py
+++ HistoriasClinicas/Historias/apps.py
@@ -4,7 +4,7 @@ from django.apps import AppConfig
 
 class HistoriasConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "historias"
+    name = "Historias"
     path = str(Path(__file__).resolve().parent)
 
     def ready(self):
--- HistoriasClinicas/HistoriasClinicas/settings.py
+++ HistoriasClinicas/HistoriasClinicas/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'drf_spectacular',
     'corsheaders',
     'Seguridad.apps.SeguridadConfig',
-    'historias.apps.HistoriasConfig',
+    'Historias.apps.HistoriasConfig',
     'Reportes',
     'Agendas',
     'Notificaciones.apps.NotificacionesConfig',
